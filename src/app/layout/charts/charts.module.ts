70674e60972707dd549c3b5944865126e224dfd3
Krunal Mistri <krunal.mistri@streebo.com>
2020-02-18 14:14:29 +0530

    Highchart Final Changes

--- src/app/layout/charts/charts.module.ts
+++ src/app/layout/charts/charts.module.ts
@@ -5,9 +5,10 @@ import { ChartsModule as Ng2Charts } from 'ng2-charts';
 import { ChartsRoutingModule } from './charts-routing.module';
 import { ChartsComponent } from './charts.component';
 import { PageHeaderModule } from '../../shared';
+import { HighchartsChartModule } from 'highcharts-angular';
 
 @NgModule({
-    imports: [CommonModule, Ng2Charts, ChartsRoutingModule, PageHeaderModule],
+    imports: [CommonModule, Ng2Charts, ChartsRoutingModule, PageHeaderModule, HighchartsChartModule],
     declarations: [ChartsComponent]
 })
 export class ChartsModule {}
